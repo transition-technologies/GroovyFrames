dec4e40b574779e6d1c9cc5230bc567573c469c9
Marek Piechut <marek.piechut@gmail.com>
2012-11-15 11:40:06 +0100

    Extract generation code to external class

---
+++ src/frames/PropertyAccessorsGenerator.groovy
@@ -0,0 +1,11 @@
+package frames
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: piechutm
+ * Date: 11/15/12
+ * Time: 10:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class PropertyAccessorsGenerator {
+}
